ec4d00a0fca72a61103b0a8576cf7ae68de89626
kivilak <kivilakc@gmail.com>
2025-07-10 22:09:02 +0530

    clean up and restructure project layout

--- Debug/Src/006spi_tx_testing.d
+++
@@ -1,9 +0,0 @@
-Src/006spi_tx_testing.o: ../Src/006spi_tx_testing.c \
- C:/Users/User/STM32CubeIDE/workspace_1.17.0/stm32l4xx_drivers/Drivers/Inc/stm32l476xx.h \
- C:/Users/User/STM32CubeIDE/workspace_1.17.0/stm32l4xx_drivers/Drivers/Inc/stm32l476xx_gpio_driver.h \
- C:/Users/User/STM32CubeIDE/workspace_1.17.0/stm32l4xx_drivers/Drivers/Inc/stm32l476xx.h \
- C:/Users/User/STM32CubeIDE/workspace_1.17.0/stm32l4xx_drivers/Drivers/Inc/stm32l476xx_spi_driver.h
-C:/Users/User/STM32CubeIDE/workspace_1.17.0/stm32l4xx_drivers/Drivers/Inc/stm32l476xx.h:
-C:/Users/User/STM32CubeIDE/workspace_1.17.0/stm32l4xx_drivers/Drivers/Inc/stm32l476xx_gpio_driver.h:
-C:/Users/User/STM32CubeIDE/workspace_1.17.0/stm32l4xx_drivers/Drivers/Inc/stm32l476xx.h:
-C:/Users/User/STM32CubeIDE/workspace_1.17.0/stm32l4xx_drivers/Drivers/Inc/stm32l476xx_spi_driver.h:
